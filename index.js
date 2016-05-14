669ea89a09ad389e21042b6e9bdce83b739ffba9
Seth Bergman <seth.atxwebs@gmail.com>
2016-05-14 02:07:01 -0500

    added buildpacks

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var favicon = require('serve-favicon');
+var php = require('php');
 //var expressMail = require('express-mail');
 var app = express();
 
@@ -8,7 +9,8 @@ var app = express();
 
 app.use(favicon(__dirname + '/favicon.ico'));
 app.use('/assets', express.static(__dirname + '/assets'));
-//var _favicon = favicon(__dirname + '/favicon.ico');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -16,35 +18,10 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
-
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
-
-app.use('/', function (req, res) {
+app.post('/', function (req, res) {
   res.send('Got a POST request');
 });
 
-/*
-app.post('/', function (req, res) {
-  res.format({
-    'text/plain': function(){
-      res.send('hey');
-    },
-
-    'text/html': function(){
-      res.send('<p>hey</p>');
-    },
-
-    'application/json': function(){
-      res.send({ message: 'hey' });
-    },
-
-    'default': function() {
-      // log the request and respond with 406
-      res.status(406).send('Not Acceptable');
-    }
-});
-*/
 var server = app.listen(process.env.PORT | 5000, function () {
   console.log('Server running at http://0.0.0.0:' + server.address().port)
 })
