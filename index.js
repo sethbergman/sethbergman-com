5c042df61d0ced27f137eedc5a0d91f2e53f59fe
Seth Bergman <seth.atxwebs@gmail.com>
2016-05-14 03:08:40 -0500

    added buildpacks

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var favicon = require('serve-favicon');
-var php = require('php');
-//var expressMail = require('express-mail');
 var app = express();
 
+var phpExpress = require('php-express')({
+
+  // assumes php is in your PATH
+  binPath: 'php'
+});;
+
+app.engine('php', phpExpress.engine);
+app.set('view engine', 'php');
+
+// routing all .php file to php-express
+app.all(/.+\.php$/, phpExpress.router);
 
 
 app.use(favicon(__dirname + '/favicon.ico'));
 app.use('/assets', express.static(__dirname + '/assets'));
 app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
-});
-app.post('/', function (req, res) {
-  res.send('Got a POST request');
 });
 
 var server = app.listen(process.env.PORT | 5000, function () {
