8ed93c187a2879c56c1759693d5dd21685c1154a
Seth Bergman <seth.atxwebs@gmail.com>
2016-05-14 15:11:03 -0500

    changed favicon

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var app = express();
 
 app.use(favicon(__dirname + '/favicon.ico'));
 app.use('/assets', express.static(__dirname + '/assets'));
-app.use(express.static(__dirname + 'php'));
+//app.use(express.static(__dirname + 'php'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', __dirname + '/views');
