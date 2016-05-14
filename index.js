33d4da4bc4ee635245458cca7fe8dd2f0edf0d7f
Seth Bergman <seth.atxwebs@gmail.com>
2016-05-14 14:43:34 -0500

    added mail options

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var favicon = require('serve-favicon');
+var mailer = require('nodemailer');
+var contact = require('contact-you');
+var http  = require('http');
+var php = require('php');
 var app = express();
 
-var php = require('php');
-
-//app.engine('php', phpExpress);
-//app.set('view engine', 'php');
 
 app.use(favicon(__dirname + '/favicon.ico'));
 app.use('/assets', express.static(__dirname + '/assets'));
 app.use(express.static(__dirname + 'php'));
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/resume', function(request, response) {
+  response.render('pages/resume');
+});
+
+app.get('/resume/print', function(request, response) {
+  response.render('print/index');
+});
+
+app.post('/send',function(req, res, next){
+    console.log(req.body);
 });
 
 var server = app.listen(process.env.PORT | 5000, function () {
