2f249d45126392724762dc2904d94e5f252101e6
Seth Bergman <seth.atxwebs@gmail.com>
2016-05-15 05:27:03 -0500

    four

--- index.js
+++ index.js
@@ -1,24 +1,57 @@
 var express = require('express');
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var bodyParser = require('body-parser');
+//var app = require('express')();
 var favicon = require('serve-favicon');
 var nodemailer = require('nodemailer');
-var $ = require('jquery');
+//var bodyParser = require('body-parser');
+//var $ = require('jquery');
 //var http  = require('http');
-//var app = express();
+var app = express();
 
-
-
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 app.use(favicon(__dirname + '/favicon.ico'));
 app.use('/assets', express.static(__dirname + '/assets'));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+//app.use(bodyParser.urlencoded({extended: true}));
 
+var smtpTransport = nodemailer.createTransport("SMTP", {
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+    service: 'Gmail',
+    auth: {
+        // enter your gmail account
+        user: 'seth.atxwebs@gmail.com',
+        // enter your gmail password
+        pass: 'gouuvbgiwtcnnbps'
+    }
+});
+
+app.use(express.static(__dirname + '/assets'));
+
+app.get('/', function (req, res) {
+    res.sendfile('./views/pages/index.html');
+});
+
+app.get('/send', function (req, res) {
+
+    var mailOptions = {
+        to: req.query.to,
+        subject: 'Contact Form Message',
+        from: "Contact Form Request" + "<" + req.query.from + '>',
+        html:  "From: " + req.query.name + "<br>" +
+               "User's email: " + req.query.user + "<br>" +     "Message: " + req.query.text
+    }
+
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function (err, response) {
+        if (err) {
+            console.log(err);
+            res.end("error");
+        } else {
+            console.log("Message sent: " + response.message);
+            res.end("sent");
+        }
+    });
+
+});
 
 app.get('/', function(request, response) {
   response.render('pages/index');
@@ -28,45 +61,23 @@ app.get('/resume', function(request, response) {
   response.render('pages/resume');
 });
 
+app.get('/thank-you', function(request, response) {
+  response.render('pages/thank-you');
+});
+
 app.get('/resume/print', function(request, response) {
   response.render('print/index');
 });
 
-app.post('/send', function(req, res) {
-  console.log('formData called');
-  console.log(req.body);
-  var resultObject = JSON.parse(req.body.objectData);
-  console.log(resultObject);
-  res.end();
+app.post('/send', function (req, res) {
+  //res.send('Got a POST request');
+  res.redirect('/thank-you');
 });
 
+//app.post('/send', function(req, res, next) {
+//  res.redirect('/');
+//});
 
-var transporter = nodemailer.createTransport('smtps://seth.atxwebs%40gmail.com:gouuvbgiwtcnnbps@smtp.gmail.com');
-
-transporter.verify(function(error, success) {
-   if (error) {
-        console.log(error);
-   } else {
-        console.log('Server is ready to accept email');
-   }
-});
-
-var mailOptions = {
-    from: '"Connect with Seth" <connect.seth.bergman@gmail.com>', // sender address
-    to: 'hello@sethbergman.com', // list of receivers
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello Seth 🐴', // plaintext body
-    html: '<b>Hello Seth 🐴</b>' // html body
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        return console.log(error);
-    }
-    console.log('Message sent: ' + info.response);
-});
-
-
-var server = http.listen(process.env.PORT | 5000, function () {
+var server = app.listen(process.env.PORT | 5000, function () {
   console.log('Server running at http://0.0.0.0:' + server.address().port)
 })
