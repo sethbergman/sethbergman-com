074411798cf7c83b7e1488ad579522d45959327a
Seth Bergman <seth.atxwebs@gmail.com>
2017-07-17 22:15:43 -0500

    update

--- assets/js/jquery.cbpQTRotator.js
+++ assets/js/jquery.cbpQTRotator.js
@@ -1,12 +1,12 @@
 /**
  * jquery.cbpQTRotator.js v1.0.0
- * http://www.codrops.com
+ * //www.codrops.com
  *
  * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
+ * //www.opensource.org/licenses/mit-license.php
  * 
  * Copyright 2013, Codrops
- * http://www.codrops.com
+ * //www.codrops.com
  */
 ;( function( $, window, undefined ) {
 
--- assets/js/jquery.parallax-1.1.3.js
+++ assets/js/jquery.parallax-1.1.3.js
@@ -3,12 +3,12 @@ Plugin: jQuery Parallax
 Version 1.1.3
 Author: Ian Lunn
 Twitter: @IanLunn
-Author URL: http://www.ianlunn.co.uk/
-Plugin URL: http://www.ianlunn.co.uk/plugins/jquery-parallax/
+Author URL: //www.ianlunn.co.uk/
+Plugin URL: //www.ianlunn.co.uk/plugins/jquery-parallax/
 
 Dual licensed under the MIT and GPL licenses:
-http://www.opensource.org/licenses/mit-license.php
-http://www.gnu.org/licenses/gpl.html
+//www.opensource.org/licenses/mit-license.php
+//www.gnu.org/licenses/gpl.html
 */
 
 (function( $ ){
