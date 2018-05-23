7eb0cf41e1ff6bbb606a8b8f92b8f02d66880ba8
Seth Bergman <seth.atxwebs@gmail.com>
2018-05-22 21:31:42 -0500

    route is working

--- index.js
+++ index.js
@@ -1,46 +1,55 @@
-const express = require('express');
-const favicon = require('serve-favicon');
-const nodemailer = require('nodemailer');
-const bunyan = require('bunyan');
-const bodyParser = require('body-parser');
-const validator = require('express-validator');
-const $ = require('jquery');
-const http = require('http');
-const https = require('https');
-const app = express();
+const express = require('express')
+const favicon = require('serve-favicon')
+const nodemailer = require('nodemailer')
+const bunyan = require('bunyan')
+const bodyParser = require('body-parser')
+const validator = require('express-validator')
+const path = require('path')
+const fs = require('fs')
+const app = express()
 // const mailOptions = require('./mailOptions');
-app.engine('html', require('ejs').renderFile);
+app.engine('html', require('ejs').renderFile)
 app.set('/views', express.static(__dirname + '/views/pages'))
-app.set('view engine', 'html');
-app.use(favicon(__dirname + '/favicon.ico'));
-app.use('/assets', express.static(__dirname + '/assets'));
+app.set('view engine', 'html')
+app.use(favicon(__dirname + '/favicon.ico'))
+app.use('/assets', express.static(__dirname + '/assets'))
 
-app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + '/assets'))
 // app.use(bodyParser.urlencoded({
 // 	extended: false
 // }))
 
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+)
+app.use(validator())
 
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
-app.use(validator());
+app.get('/', function(request, response) {
+  response.render('pages/index')
+})
 
-app.get('/', function (request, response) {
-	response.render('pages/index');
-});
+app.get('/resume', function(req, res) {
+  var filePath = '/views/pages/Seth-Bergman-Resume.pdf'
 
-app.get('/resume', function (request, response) {
-	response.sendFile('pages/Seth-Bergman-Resume.pdf');
-});
+  fs.readFile(__dirname + filePath, function(err, data) {
+    res.contentType('application/pdf')
+    res.send(data)
+  })
+})
 
-app.get('/thank-you', function (request, response) {
-	response.render('pages/thank-you');
-});
+// app.get('/resume', function (request, response) {
+// 	response.render('pages/Seth-Bergman-Resume.pdf');
+// });
 
-app.get('/resume/print', function (request, response) {
-	response.render('print/index');
-});
+app.get('/thank-you', function(request, response) {
+  response.render('pages/thank-you')
+})
+
+app.get('/resume/print', function(request, response) {
+  response.render('print/index')
+})
 
 /*
 app.get('/send', function (req, res) {
@@ -64,64 +73,69 @@ app.get('/send', function (req, res) {
 //   }
 // });
 
-var smtpTransport = nodemailer.createTransport("SMTP", {
+var smtpTransport = nodemailer.createTransport('SMTP', {
+  service: 'Gmail',
+  auth: {
+    // enter your gmail account
+    user: 'seth.atxwebs@gmail.com',
+    // enter your gmail password
+    pass: 'gouuvbgiwtcnnbps',
+  },
+})
 
-	service: 'Gmail',
-	auth: {
-		// enter your gmail account
-		user: 'seth.atxwebs@gmail.com',
-		// enter your gmail password
-		pass: 'gouuvbgiwtcnnbps'
-	}
-});
+app.post('/', function(req, res) {
+  req.assert('name', 'Name is required').notEmpty() //Validate name
+  req.assert('email', 'A valid email is required').isEmail() //Validate email
 
-app.post('/', function (req, res) {
-	req.assert('name', 'Name is required').notEmpty(); //Validate name
-	req.assert('email', 'A valid email is required').isEmail(); //Validate email
+  var errors = req.getValidationResult()
+  if (!errors) {
+    //No errors were found.  Passed Validation!
+    res.render('pages/index.html', {
+      title: 'Form Validation Example',
+      message: 'Passed Validation!',
+      errors: {},
+    })
+  } else {
+    //Display errors to user
+    res.render('pages/index.html', {
+      title: 'Form Validation Example',
+      message: '',
+      errors: errors,
+    })
+  }
+})
+//
+app.get('/', function(req, res) {
+  res.sendfile('pages/index.html')
+})
 
-	var errors = req.getValidationResult();
-	if (!errors) { //No errors were found.  Passed Validation!
-		res.render('pages/index.html', {
-			title: 'Form Validation Example',
-			message: 'Passed Validation!',
-			errors: {}
-		});
-
-	} else { //Display errors to user
-		res.render('pages/index.html', {
-			title: 'Form Validation Example',
-			message: '',
-			errors: errors
-		});
-	}
-});
-// 
-app.get('/', function (req, res) {
-	res.sendfile('pages/index.html');
-});
-
-app.get('/send', function (req, res) {
-
-    var mailOptions = {
-        to: req.query.to,
-        subject: 'Contact Form Message',
-        from: "Contact Form Request" + "<" + req.query.from + '>',
-        html:  "From: " + req.query.name + "<br>" +
-               "User's email: " + req.query.user + "<br>" +     "Message: " + req.query.text
-    }
+app.get('/send', function(req, res) {
+  var mailOptions = {
+    to: req.query.to,
+    subject: 'Contact Form Message',
+    from: 'Contact Form Request' + '<' + req.query.from + '>',
+    html:
+      'From: ' +
+      req.query.name +
+      '<br>' +
+      "User's email: " +
+      req.query.user +
+      '<br>' +
+      'Message: ' +
+      req.query.text,
+  }
 
   //  console.log(mailOptions);
-    smtpTransport.sendMail(mailOptions, function (err, response) {
-        if (err) {
-          //  console.log(err);
-            res.end("error");
-        } else {
-          //  console.log("Message sent: " + response.message);
-            res.end("sent");
-        }
-    });
-
-});
+  smtpTransport.sendMail(mailOptions, function(err, response) {
+    if (err) {
+      //  console.log(err);
+      res.end('error')
+    } else {
+      //  console.log("Message sent: " + response.message);
+      res.end('sent')
+    }
+  })
+})
 // app.get('/modal1', function (req, res, next) {
 // 		var mailOptions = {
 // 		to: req.query.to,
@@ -130,11 +144,10 @@ app.get('/send', function (req, res) {
 // 		html: "From: " + req.query.name + "<br>" +
 // 			"User's email: " + req.query.user + "<br>" + "Message: " + req.query.text
 // 	}
-// 
+//
 // 	// res.render('pages/thank-you');
 // });
 
-
-const server = app.listen(process.env.PORT || 5000, function () {
-	console.log('Server running at http://0.0.0.0:' + server.address().port)
+const server = app.listen(process.env.PORT || 5000, function() {
+  console.log('Server running at http://0.0.0.0:' + server.address().port)
 })
