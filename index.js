c53cb6e469f26a63532a9f4144eef423b2078ea1
Seth Bergman <seth.atxwebs@gmail.com>
2018-12-27 05:09:25 -0600

    On branch master Changes to be committed: modified:   index.js modified:   mailOptions.js deleted:    yarn.lock

--- index.js
+++ index.js
@@ -1,55 +1,47 @@
-const express = require('express')
-const favicon = require('serve-favicon')
-const nodemailer = require('nodemailer')
-const bunyan = require('bunyan')
-const bodyParser = require('body-parser')
-const validator = require('express-validator')
-const path = require('path')
-const fs = require('fs')
-const app = express()
+const express = require("express");
+const favicon = require("serve-favicon");
+const nodemailer = require("nodemailer");
+const bunyan = require("bunyan");
+const bodyParser = require("body-parser");
+const validator = require("express-validator");
+const path = require("path");
+const fs = require("fs");
+const app = express();
 // const mailOptions = require('./mailOptions');
-app.engine('html', require('ejs').renderFile)
-app.set('/views', express.static(__dirname + '/views/pages'))
-app.set('view engine', 'html')
-app.use(favicon(__dirname + '/favicon.ico'))
-app.use('/assets', express.static(__dirname + '/assets'))
+app.engine("html", require("ejs").renderFile);
+app.set("/views", express.static(__dirname + "/views/pages"));
+app.set("view engine", "html");
+app.use(favicon(__dirname + "/favicon.ico"));
+app.use("/assets", express.static(__dirname + "/assets"));
 
-app.use(express.static(__dirname + '/assets'))
-// app.use(bodyParser.urlencoded({
-// 	extended: false
-// }))
-
+app.use(express.static(__dirname + "/assets"));
 app.use(
   bodyParser.urlencoded({
-    extended: false,
+    extended: false
   })
-)
-app.use(validator())
+);
+app.use(validator());
 
-app.get('/', function(request, response) {
-  response.render('pages/index')
-})
+app.get("/", function(request, response) {
+  response.render("pages/index");
+});
 
-app.get('/resume', function(req, res) {
-  var filePath = '/views/pages/Seth-Bergman-Resume.pdf'
+app.get("/resume", function(req, res) {
+  var filePath = "/views/pages/Seth-Bergman-Resume.pdf";
 
   fs.readFile(__dirname + filePath, function(err, data) {
-    res.contentType('application/pdf')
-    res.send(data)
-  })
-})
+    res.contentType("application/pdf");
+    res.send(data);
+  });
+});
 
-// app.get('/resume', function (request, response) {
-// 	response.render('pages/Seth-Bergman-Resume.pdf');
-// });
+app.get("/thank-you", function(request, response) {
+  response.render("pages/thank-you");
+});
 
-app.get('/thank-you', function(request, response) {
-  response.render('pages/thank-you')
-})
-
-app.get('/resume/print', function(request, response) {
-  response.render('print/index')
-})
+app.get("/resume/print", function(request, response) {
+  response.render("print/index");
+});
 
 /*
 app.get('/send', function (req, res) {
@@ -73,69 +65,91 @@ app.get('/send', function (req, res) {
 //   }
 // });
 
-var smtpTransport = nodemailer.createTransport('SMTP', {
-  service: 'Gmail',
-  auth: {
-    // enter your gmail account
-    user: 'seth.atxwebs@gmail.com',
-    // enter your gmail password
-    pass: 'gouuvbgiwtcnnbps',
+// var smtpTransport = nodemailer.createTransport('SMTP', {
+//   service: 'Gmail',
+//   auth: {
+//     // enter your gmail account
+//     user: 'seth.atxwebs@gmail.com',
+//     // enter your gmail password
+//     pass: 'gouuvbgiwtcnnbps',
+//   },
+// })
+
+var smtpTransport = nodemailer.createTransport(
+  {
+    service: "Gmail",
+    auth: {
+      type: "SMTP",
+      user: "seth.atxwebs@gmail.com",
+      pass: "gouuvbgiwtcnnbps"
+      // expires: 12345
+    },
+    debug: false // include SMTP traffic in the logs
   },
-})
+  {
+    // default message fields
 
-app.post('/', function(req, res) {
-  req.assert('name', 'Name is required').notEmpty() //Validate name
-  req.assert('email', 'A valid email is required').isEmail() //Validate email
+    // sender info
+    from: "StackRiot <stackriot@gmail.com>",
+    headers: {
+      "X-Laziness-level": 1000 // just an example header, no need to use this
+    }
+  }
+);
 
-  var errors = req.getValidationResult()
+app.post("/", function(req, res) {
+  req.assert("name", "Name is required").notEmpty(); //Validate name
+  req.assert("email", "A valid email is required").isEmail(); //Validate email
+
+  var errors = req.getValidationResult();
   if (!errors) {
     //No errors were found.  Passed Validation!
-    res.render('pages/index.html', {
-      title: 'Form Validation Example',
-      message: 'Passed Validation!',
-      errors: {},
-    })
+    res.render("pages/index.html", {
+      title: "Form Validation Example",
+      message: "Passed Validation!",
+      errors: {}
+    });
   } else {
     //Display errors to user
-    res.render('pages/index.html', {
-      title: 'Form Validation Example',
-      message: '',
-      errors: errors,
-    })
+    res.render("pages/index.html", {
+      title: "Form Validation Example",
+      message: "",
+      errors: errors
+    });
   }
-})
+});
 //
-app.get('/', function(req, res) {
-  res.sendfile('pages/index.html')
-})
+app.get("/", function(req, res) {
+  res.sendfile("pages/index.html");
+});
 
-app.get('/send', function(req, res) {
+app.get("/send", function(req, res) {
   var mailOptions = {
     to: req.query.to,
-    subject: 'Contact Form Message',
-    from: 'Contact Form Request' + '<' + req.query.from + '>',
+    subject: "Contact Form Message",
+    from: "Contact Form Request" + "<" + req.query.from + ">",
     html:
-      'From: ' +
+      "From: " +
       req.query.name +
-      '<br>' +
+      "<br>" +
       "User's email: " +
       req.query.user +
-      '<br>' +
-      'Message: ' +
-      req.query.text,
-  }
+      "<br>" +
+      "Message: " +
+      req.query.text
+  };
 
   //  console.log(mailOptions);
   smtpTransport.sendMail(mailOptions, function(err, response) {
     if (err) {
       //  console.log(err);
-      res.end('error')
+      res.end("error");
     } else {
       //  console.log("Message sent: " + response.message);
-      res.end('sent')
+      res.end("sent");
     }
-  })
-})
+  });
+});
 // app.get('/modal1', function (req, res, next) {
 // 		var mailOptions = {
 // 		to: req.query.to,
@@ -149,5 +163,5 @@ app.get('/send', function(req, res) {
 // });
 
 const server = app.listen(process.env.PORT || 5000, function() {
-  console.log('Server running at http://0.0.0.0:' + server.address().port)
-})
+  console.log("Server running at http://0.0.0.0:" + server.address().port);
+});
--- mailOptions.js
+++ mailOptions.js
@@ -1,75 +1,78 @@
-require('./index.js');
-var bunyan = require('bunyan');
-var nodemailer = require('nodemailer');
-var app = require('express');
+require("./index.js");
+var bunyan = require("bunyan");
+var nodemailer = require("nodemailer");
+var app = require("express");
 // module.exports = function(mail, auth) {
-  var bunyan = require('bunyan');
-  var nodemailer = require('nodemailer');
-  var logger = bunyan.createLogger({
-      name: 'nodemailer'
-  });
-logger.level('trace');
-var transporter = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-      type: 'SMTP',
-      user: 'seth.atxwebs@gmail.com',
-      pass: 'gouuvbgiwtcnnbps'
-      // expires: 12345
-  },
-  logger,
-  debug: false // include SMTP traffic in the logs
-}, {
-  // default message fields
-
-  // sender info
-  from: 'StackRiot <stackriot@gmail.com>',
-  headers: {
-      'X-Laziness-level': 1000 // just an example header, no need to use this
-  }
+var bunyan = require("bunyan");
+var nodemailer = require("nodemailer");
+var logger = bunyan.createLogger({
+  name: "nodemailer"
 });
-// 
+logger.level("trace");
+var transporter = nodemailer.createTransport(
+  {
+    service: "Gmail",
+    auth: {
+      type: "SMTP",
+      user: "seth.atxwebs@gmail.com",
+      pass: "gouuvbgiwtcnnbps"
+      // expires: 12345
+    },
+    logger,
+    debug: false // include SMTP traffic in the logs
+  },
+  {
+    // default message fields
+
+    // sender info
+    from: "StackRiot <stackriot@gmail.com>",
+    headers: {
+      "X-Laziness-level": 1000 // just an example header, no need to use this
+    }
+  }
+);
+//
 // console.log('SMTP Configured');
-// 
+//
 // // Message object
 // var message = {
-// 
+//
 //   // Comma separated list of recipients
 //   to: 'Seth Bergman <seth.atxwebs@gmail.com>',
-// 
+//
 //   // Subject of the message
 //   subject: 'Nodemailer is unicode friendly ✔ #', //
-// 
+//
 //   // plaintext body
 //   text: 'Hello to myself!',
-// 
+//
 //   // HTML body
 //   html: '<p><b>Hello</b> to myself <img src="cid:note@example.com"/></p>' +
 //       '<p>Here\'s a nyan cat for you as an embedded attachment:<br/><img src="cid:nyan@example.com"/></p>',
-// 
+//
 //   // Apple Watch specific HTML body
 //   watchHtml: '<b>Hello</b> to myself',
-// 
+//
 //   // An array of attachments
 //   attachments: [
-// 
+//
 //       // String attachment
 //       {
 //           filename: 'notes.txt',
 //           content: 'Some notes about this e-mail',
 //           contentType: 'text/plain' // optional, would be detected from the filename
 //       },
-// 
+//
 //       // Binary Buffer attachment
 //       {
 //           filename: 'image.png',
 //           content: new Buffer('iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUAAAD/' +
 //               '//+l2Z/dAAAAM0lEQVR4nGP4/5/h/1+G/58ZDrAz3D/McH8yw83NDDeNGe4U' +
 //               'g9C9zwz3gVLMDA/A6P9/AFGGFyjOXZtQAAAAAElFTkSuQmCC', 'base64'),
-// 
+//
 //           cid: 'note@example.com' // should be as unique as possible
 //       },
-// 
+//
 //       // File Stream attachment
 //       {
 //           filename: 'nyan cat ✔.gif',
@@ -78,18 +81,18 @@ var transporter = nodemailer.createTransport({
 //       }
 //   ]
 // };
-console.log('Sending Mail');
+console.log("Sending Mail");
 transporter.sendMail(message, (error, info) => {
   if (error) {
-      console.log('Error occurred');
-      console.log(error.message);
-      return;
+    console.log("Error occurred");
+    console.log(error.message);
+    return;
   }
-  console.log('Message sent successfully!');
-  console.log('Server responded with "%s"', info.response)
-      console.log('Message sent successfully!');
-      console.log('Server responded with "%s"', info.response);
-      transporter.close();
+  console.log("Message sent successfully!");
+  console.log('Server responded with "%s"', info.response);
+  console.log("Message sent successfully!");
+  console.log('Server responded with "%s"', info.response);
+  transporter.close();
 });
 module.exports = transporter;
 // 	mailOptions: {
@@ -107,7 +110,6 @@ module.exports = transporter;
 // 		pass: 'gouuvbgiwtcnnbps'
 // 	}
 // }
-
 
 // Create a SMTP transporter object
 // var transporter = nodemailer.createTransport({
